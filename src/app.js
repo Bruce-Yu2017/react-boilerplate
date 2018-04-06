93d5c11f6da15d59ccb99e2b70f6277ddd6c26da
Bruce-Yu2017 <ilywanzi@gmail.com>
2018-04-05 18:57:10 -0700

    boilerplate

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./style/style.scss"
+
+ReactDOM.render(<p>new app</p>, document.getElementById("app"));
+
